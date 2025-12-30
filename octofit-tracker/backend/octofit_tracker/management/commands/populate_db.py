42e172fa463927a65baf20867878c88de300a4da
sunaina182024 <sunaina.patil@accenture.com>
2025-12-30 13:53:39 +0000

    Add management command to populate octofit_db with test data

---
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -0,0 +1,41 @@
+from django.core.management.base import BaseCommand
+from octofit_tracker.models import User, Team, Activity, Leaderboard, Workout
+
+class Command(BaseCommand):
+    help = 'Populate the octofit_db database with test data'
+
+    def handle(self, *args, **kwargs):
+        # Delete existing data
+        User.objects.all().delete()
+        Team.objects.all().delete()
+        Activity.objects.all().delete()
+        Leaderboard.objects.all().delete()
+        Workout.objects.all().delete()
+
+        # Create Teams
+        marvel = Team.objects.create(name='Team Marvel')
+        dc = Team.objects.create(name='Team DC')
+
+        # Create Users
+        tony = User.objects.create(name='Tony Stark', email='tony@marvel.com', team=marvel)
+        steve = User.objects.create(name='Steve Rogers', email='steve@marvel.com', team=marvel)
+        bruce = User.objects.create(name='Bruce Wayne', email='bruce@dc.com', team=dc)
+        clark = User.objects.create(name='Clark Kent', email='clark@dc.com', team=dc)
+
+        # Create Activities
+        Activity.objects.create(user=tony, type='Running', duration=30)
+        Activity.objects.create(user=steve, type='Cycling', duration=45)
+        Activity.objects.create(user=bruce, type='Swimming', duration=60)
+        Activity.objects.create(user=clark, type='Yoga', duration=20)
+
+        # Create Workouts
+        Workout.objects.create(name='Super Strength', description='Strength training for superheroes')
+        Workout.objects.create(name='Flight Training', description='Aerobic workout for flying heroes')
+
+        # Create Leaderboard
+        Leaderboard.objects.create(user=tony, score=100)
+        Leaderboard.objects.create(user=steve, score=90)
+        Leaderboard.objects.create(user=bruce, score=95)
+        Leaderboard.objects.create(user=clark, score=98)
+
+        self.stdout.write(self.style.SUCCESS('Test data populated successfully.'))
